7c5c2c257c91446f4c7ab945786165d506fb73fc
Zicong-Huang <jacobhuangzicong@gmail.com>
2021-01-11 04:15:19 -0500

    minor modifications

--- neural_plot_digits.py
+++ neural_plot_digits.py
@@ -1,10 +1,8 @@
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def draw_random_gray_scale_digit():
-    gray_scale_data = pd.read_csv('../ml_python/sample_data/MNIST.csv', header=None).to_numpy()
+def draw_random_gray_scale_digit(gray_scale_data, size=(20, 20)):
     row, col = gray_scale_data.shape
 
     digit_gray_scale = gray_scale_data[:, :col - 1]
@@ -16,12 +14,13 @@ def draw_random_gray_scale_digit():
     for i in range(3):
         for j in range(3):
             x = np.random.randint(row)
-            digit = digit_gray_scale[x].reshape((20, 20))
-            axs[i, j].imshow(digit, cmap='gray')
+            digit = digit_gray_scale[x].reshape(size)
+            axs[i, j].imshow(digit.T, cmap='gray')  # note that pixels lay out in a top-bottom, left-right manner
             axs[i, j].get_xaxis().set_ticks([])
             axs[i, j].get_yaxis().set_ticks([])
             axs[i, j].set_xlabel(int(digit_label[x]))
     fig1.suptitle('Examples of handwritten digits in gray scale')
+    plt.pause(0.1)
 
 
 if __name__ == '__main__':
